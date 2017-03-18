818f921eec4b8e31213ed70ae9a25bd19e23050c
ljyockey <linyockey@gmail.com>
2017-03-17 20:40:07 -0700

    finished most of the functions

--- app.js
+++ app.js
@@ -1,42 +1,71 @@
 // your code here!
 
-function normalizeText(text) {
-	var newText = text.split().toLowerCase();
-	return newText;
-};
+// function normalizeText(text) {
+// 	var newText = text.toLowerCase();
+// 	newText.split(/[ ,!.";:-]/).sort();
+// 	return newText;
+// 	//not feeling super confident about this split
+// };
 
 function wordCount(text) {
-	return text.length();
+	return text.length;
 };
 
 function uniqueWordCount(text) {
-//still need to do this
+ /*We start with a variable that stores the counter. Then we need
+ to see if a particular item in the array is in the array. If it is not, 1 can
+ be added to the counter*/
+ 	var result = 0;
+ 	for (var i = 0; i < text.length; i++) {
+ 		if (text[i] !== text[i+1] && text[i] !== text[i-1]) {
+ 			result += 1;
+ 		}
+ 	}
+ 	return result;
 };
 
 function averageWordLength(text) {
 	var lengthResult = 0;
 	text.forEach(function(item) {
-		lengthResult += item.length();
+		lengthResult += item.length;
 		//create average and return it
 	});
+	return lengthResult/text.length;
 
 };
 
-$("button").click(function() {
+$("button").click(function(e) {
+	e.preventDefault();
 	$(".hidden").show();
 	var input = $(".js-input").val();
-	normalizeText(input);
-	$(".js-count").html(wordCount(newText));
-	$(".js-unique").html(uniqueWordCount(newText));
-	$(".js-average").html(averageWordLength(newText));
+	input = input.toLowerCase().replace(/[^a-z]/g, " ").trim();
+	input = input.split(" ").sort();
+	for (var i = 0; i < input.length; i++) {
+		if (input[i] === " ") {
+			input.splice(i, 1);
+		}
+		return input;
+	}
+	console.log(input);
+	// normalizeText(input);
+	$(".js-count").html(wordCount(input));
+	$(".js-unique").html(uniqueWordCount(input));
+	$(".js-average").html(averageWordLength(input));
 });
 
-/* First, we need to return a string that is all lowercase and no special
+/*First, we need to return a string that is all lowercase and no special
 characters. That string (or array?) will become the variable that is used as the
 input for each of the rest of the functions. Then word count will just be that
-word variable.length - 1; and we'll return that value as a variable that can be
+word variable.length; and we'll return that value as a variable that can be
 used later. Unique word count will require a counter that will add 1 ONLY if the
 current item in the array !== any other item within the array. The 
 averageWordLength function will require a counter that takes the length of each
 item in the array and then divides that by the number of items in array.
 Will splice return an array if I use it in normalize text??
+
+Ground Control to Major Tom
+Ground Control to Major Tom
+Take your protein pills and put your helmet on
+Ground Control to Major Tom (ten, nine, eight, seven, six)
+Commencing countdown, engines on (five, four, three)
+Check ignition and may God's love be with you (two, one, liftoff)*/
